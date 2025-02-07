39153988b3eb26baa60c7cd21d1f3d7530e4b066
Zainulobiddin <zainiddin893@gmail.com>
2025-02-07 16:17:13 +0500

    .

--- main.js
+++ main.js
@@ -59,7 +59,7 @@
 
 // function century(year) {
 //     if (year%100 == 0) {
-//         return ~~(year/100)
+//         return (year/100)
 //     } else if (year%100 != 0) {
 //         return (~~(year/100))+1
 //     }
